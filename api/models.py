209f86992ff06aec5cc4f83f346717837a53a90c
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2022-03-01 17:52:55 -0500

    feature: added models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    
+    @property
+    def movies_as_actor(self):
+        movies = Movie.objects.filter(person__id=self.id).filter(members__role='A')
+        return movies
+
+    @property
+    def movies_as_director(self):
+        movies = Movie.objects.filter(person__id=self.id).filter(members__role='D')
+        return movies
+
+    @property
+    def movies_as_producer(self):
+        movies = Movie.objects.filter(person__id=self.id).filter(members__role='P')
+        return movies
+
+class Alias(models.Model):
+    name = models.CharField(max_length=200)
+    person = models.ForeignKey(
+        Person,
+        on_delete=models.CASCADE,
+    )
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    release_year = models.SmallIntegerField()
+    members = models.ManyToManyField(Person,
+        through='Role',
+        through_fields=('movie', 'person'))
+
+    @property
+    def actors(self):
+        persons = Person.objects.filter(movie__id=self.id).filter(members__role='A')
+        return persons
+
+    @property
+    def directors(self):
+        persons = Person.objects.filter(movie__id=self.id).filter(members__role='D')
+        return persons
+
+    @property
+    def producers(self):
+        persons = Person.objects.filter(movie__id=self.id).filter(members__role='P')
+        return persons
+
+class Role(models.Model):
+    PERSON_MOVIE_ROLES = [
+        ('A', 'Actor/Actress'),
+        ('D', 'Director'),
+        ('P', 'Producer')
+    ]
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    role = models.CharField(max_length=1, choices=PERSON_MOVIE_ROLES)
