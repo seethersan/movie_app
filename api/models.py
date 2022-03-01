5af8a937054d70a6e962ade2bcc8085f1b5a21de
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2022-03-01 18:44:15 -0500

    fix: properties filters

--- api/models.py
+++ api/models.py
@@ -6,17 +6,17 @@ class Person(models.Model):
     
     @property
     def movies_as_actor(self):
-        movies = Movie.objects.filter(person__id=self.id).filter(members__role='A')
+        movies = self.movie_set.filter(movie_person__role='A').get()
         return movies
 
     @property
     def movies_as_director(self):
-        movies = Movie.objects.filter(person__id=self.id).filter(members__role='D')
+        movies = self.movie_set.filter(movie_person__role='D').get()
         return movies
 
     @property
     def movies_as_producer(self):
-        movies = Movie.objects.filter(person__id=self.id).filter(members__role='P')
+        movies = self.movie_set.filter(movie_person__role='P').get()
         return movies
 
 class Alias(models.Model):
@@ -35,17 +35,17 @@ class Movie(models.Model):
 
     @property
     def actors(self):
-        persons = Person.objects.filter(movie__id=self.id).filter(members__role='A')
+        persons = self.members.filter(movie_person__role='A').get()
         return persons
 
     @property
     def directors(self):
-        persons = Person.objects.filter(movie__id=self.id).filter(members__role='D')
+        persons = self.members.filter(movie_person__role='D').get()
         return persons
 
     @property
     def producers(self):
-        persons = Person.objects.filter(movie__id=self.id).filter(members__role='P')
+        persons = self.members.filter(movie_person__role='P').get()
         return persons
 
 class Role(models.Model):
@@ -54,6 +54,6 @@ class Role(models.Model):
         ('D', 'Director'),
         ('P', 'Producer')
     ]
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='movie_person')
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='movie_person')
     role = models.CharField(max_length=1, choices=PERSON_MOVIE_ROLES)
